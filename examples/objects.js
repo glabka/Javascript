f3723c9cc187d473f76a50e221a1903ef942d81c
glabka <glabka333@gmail.com>
2022-03-23 22:08:28 +0100

    examples of object creation and inheritance.

---
+++ examples/objects.js
@@ -0,0 +1,60 @@
+var flyFunction = function() {
+    console.log('flying');
+};
+flyFunction();
+
+var superman = {
+    heroName : 'SuperMan',
+    sayHello : function() {
+        console.log('Hello I\'m ' + this.heroName);
+    }
+};
+
+superman.sayHello(); // Hello I'm SuperMan
+
+var failThis = superman.sayHello;
+failThis(); // Hello, I'm undefined
+
+var hello = failThis;
+hello.call(superman); // Hello I'm SuperMan
+
+// not preferred way of instatiating objects through constructor function
+function SuperHero(name, strength) {
+    this.name = name;
+    this.strength = strength;
+};
+
+var batman = new SuperHero('Batman', 100);
+console.log('Power of ' + batman.name + ' is ' + batman.strength);
+
+//-------------------
+// Inheritance
+//-------------------
+var avangersHero = {
+    editor : 'Marvel'
+};
+
+var ironMan = {};
+
+// adding to prototype of instance
+ironMan.__proto__ = avangersHero;
+
+console.log('Iron Man is copyrighted by ' + ironMan.editor);
+
+// adding to constructor functions prototype - not recommended by https://blog.angular-university.io/javascript-for-java-developers/
+SuperHero.prototype.sayHi = function() {
+    console.log('Hi');
+};
+batman.sayHi();
+
+// recommended way
+var superHeroPrototype = {
+    sayHello : function() {
+        console.log('Hello, my name is ' + this.name);
+    }
+}
+
+var flash = Object.create(superHeroPrototype);
+flash.name = 'Flash';
+
+flash.sayHello();
